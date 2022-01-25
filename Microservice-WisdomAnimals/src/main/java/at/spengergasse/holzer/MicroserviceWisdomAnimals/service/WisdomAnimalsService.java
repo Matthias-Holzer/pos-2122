683429c379658dce6f7bfe68b72728cf437b7c31
Matthias-Holzer <wien.matthias.holzer@gmail.com>
2022-01-25 15:12:18 +0100

    s1-f5: added circuit breaker

--- Microservice-WisdomAnimals/src/main/java/at/spengergasse/holzer/MicroserviceWisdomAnimals/service/WisdomAnimalsService.java
+++ Microservice-WisdomAnimals/src/main/java/at/spengergasse/holzer/MicroserviceWisdomAnimals/service/WisdomAnimalsService.java
@@ -6,9 +6,13 @@ import at.spengergasse.holzer.MicroserviceWisdomAnimals.services.client.AnimalDt
 import at.spengergasse.holzer.MicroserviceWisdomAnimals.services.client.QuoteClient;
 import at.spengergasse.holzer.MicroserviceWisdomAnimals.services.client.QuoteDto;
 import at.spengergasse.holzer.MicroserviceWisdomAnimals.services.WisdomAnimalDto;
+import feign.Response;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
@@ -20,6 +24,10 @@ public class WisdomAnimalsService {
     private final QuoteClient quoteClient;
     private final AnimalClient animalClient;
 
+    private static final String RESILIENCE4J_INSTANCE_NAME = "wa-circut_breaker";
+    private static final String FALLBACK_METHOD = "fallback";
+
+    @CircuitBreaker(name = RESILIENCE4J_INSTANCE_NAME, fallbackMethod = FALLBACK_METHOD)
     public WisdomAnimalDto generateWAFor(long animalNumber, long quoteNumber){
         Optional<AnimalDto> animalDto = animalClient.findOne(animalNumber);
         Optional<QuoteDto> quoteDto = quoteClient.findOne(quoteNumber);
@@ -34,6 +42,7 @@ public class WisdomAnimalsService {
         return wisdomAnimalDto.get();
     }
 
+    @CircuitBreaker(name = RESILIENCE4J_INSTANCE_NAME, fallbackMethod = FALLBACK_METHOD)
     public WisdomAnimalDto generateWAForRandom(){
         Optional<AnimalDto> animalDto = animalClient.random();
         Optional<QuoteDto> quoteDto = quoteClient.random();
@@ -45,5 +54,33 @@ public class WisdomAnimalsService {
                 .textQuote(quoteDto.get().getText())
                 .build());
         return wisdomAnimalDto.get();
+    }
+
+    public WisdomAnimalDto fallback(Exception ex) {
+        return WisdomAnimalDto.builder()
+                .artAnimal("Envoyer par email \t\n" +
+                        "\n" +
+                        "             ,\n" +
+                        "            /|      __\n" +
+                        "           / |   ,-~ /\n" +
+                        "          Y :|  //  /\n" +
+                        "          | jj /( .^\n" +
+                        "          >-\"~\"-v\"\n" +
+                        "         /       Y\n" +
+                        "        jo  o    |\n" +
+                        "       ( ~T~     j\n" +
+                        "        >._-' _./\n" +
+                        "       /   \"~\"  |\n" +
+                        "      Y     _,  |\n" +
+                        "     /| ;-\"~ _  l\n" +
+                        "    / l/ ,-\"~    \\\n" +
+                        "    \\//\\/      .- \\\n" +
+                        "     Y        /    Y    -Row\n" +
+                        "     l       I     !\n" +
+                        "     ]\\      _\\    /\"\\\n" +
+                        "    (\" ~----( ~   Y.  )\n" +
+                        "~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                .textQuote("Sometimes things do not work out and the reason has nothing to do with you, or how good you are")
+                .build();
     }
 }
